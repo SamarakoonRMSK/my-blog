42e3b6c312b4c710843d4ee9d5099b69b59d76af
Rathnayake99 <rmsksamarakoon@gmail.com>
2024-06-06 17:35:41 +0530

    dark theme added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import Theme from "./components/Theme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <Theme>
+        <App />
+      </Theme>
     </React.StrictMode>
   </Provider>
 );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSliceReducer from "./reducers/userSlice";
+import themeSliceReducer from "./theme/themeSlice";
 
 const store = configureStore({
   reducer: {
     userSlice: userSliceReducer,
+    themeSlice: themeSliceReducer,
   },
 });
 
