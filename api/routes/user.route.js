d3a83d0f5c5daac5b0ff1c0a60d571e460bdf41e
Rathnayake99 <rmsksamarakoon@gmail.com>
2024-06-12 23:08:47 +0530

    route controllers add

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -14,6 +14,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.get("/getusers", verifyToken, getusers);
 router.post("/signout", signout);
 router.delete("/delete/:userId", verifyToken, deleteUser);
-router.get("/:userId", verifyToken, getUser);
+router.get("/:userId", getUser);
 
 export default router;
